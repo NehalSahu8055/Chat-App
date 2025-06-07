396110c0f3d7431d03fed3e7150ad2fc19b2bb6a
NEHALXHONOR <professionalnehal@outlook.com>
2025-06-07 14:47:26 +0530

    Fix: imports

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ dotenv.config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-
+const path = require("path");
 const app = express();
 
 const corsOptions = {
